ffbe94958ae01f1866ee4595be14c2430bf05d3c
mingeun.kim <mingeun.kim@navercorp.com>
2025-01-19 17:44:01 +0900

    build: vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MADII-ADMIN/', // GitHub Pages의 서브 디렉토리 경로
 })
